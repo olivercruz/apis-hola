7d78007841b5172d095b1de27140be8d1dc21fa9
oliver <ronaldo10oliver@gmail.com>
2022-02-07 17:28:31 -0600

    agregando valores

--- src/main/java/mx/com/telefonica/apis/controller/HolaController.java
+++ src/main/java/mx/com/telefonica/apis/controller/HolaController.java
@@ -1,13 +1,22 @@
 package mx.com.telefonica.apis.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/mensajes")
 public class HolaController {
 	
+	private final Logger logger = LoggerFactory.getLogger(HolaController.class);
+	
 	@GetMapping("/hola")
+	@CrossOrigin
 	public String getHola() {
+		logger.info("++ IMPRIMIENDO MENSAJE +++");
 		return "Hola Prueba";
 	}
 }
