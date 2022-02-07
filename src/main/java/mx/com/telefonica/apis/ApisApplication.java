e1686aebb914b41a2835fc7933c4ff74c06fdc31
oliver <ronaldo10oliver@gmail.com>
2022-02-06 22:28:01 -0600

    subiendo hola mundo

---
+++ src/main/java/mx/com/telefonica/apis/ApisApplication.java
@@ -0,0 +1,12 @@
+package mx.com.telefonica.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisApplication.class, args);
+	}
+}
