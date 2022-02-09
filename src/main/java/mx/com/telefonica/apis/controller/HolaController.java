ece99be8ac2cd8e8e564e6cd9ce045998484ddd4
oliver <ronaldo10oliver@gmail.com>
2022-02-08 22:10:23 -0600

    agregand mensaje en variable

--- src/main/java/mx/com/telefonica/apis/controller/HolaController.java
+++ src/main/java/mx/com/telefonica/apis/controller/HolaController.java
@@ -2,6 +2,8 @@ package mx.com.telefonica.apis.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +15,18 @@ public class HolaController {
 	
 	private final Logger logger = LoggerFactory.getLogger(HolaController.class);
 	
+	@Autowired
+	private Environment env;
+
 	@GetMapping("/hola")
 	@CrossOrigin
 	public String getHola() {
 		logger.info("++ IMPRIMIENDO MENSAJE +++");
+		String mensaje = null;
+
+		mensaje = env.getProperty("SPRING_MENSAJE");
+		logger.info(" ++ MENSAJE +++ --> " + mensaje);
+
 		return "Hola Prueba";
 	}
 }
